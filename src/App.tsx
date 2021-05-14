d6d921909df91ca68473b4790390be55a5a5c576
Vadx <lv_km@ukr.net>
2021-05-14 13:52:32 +0300

    Add starter

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '@material-ui/core/Container'
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <header>
+        start
+      </header>
+    </Container>
+  );
+}
+
+export default App
